0fb8ac994a1e23ca5c2caa516cd6138e8cca4408
thanveera <thanveerathasni27@gmail.com>
2026-01-26 18:44:45 +0530

    add axios request interceptor reading access token from redux

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import type { AxiosInstance } from "axios";
+import { store } from "../store/store";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
@@ -9,4 +10,19 @@ export const api: AxiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+// --------------------
+// REQUEST INTERCEPTOR
+// --------------------
+api.interceptors.request.use((config) => {
+  const state = store.getState();
+  const accessToken = state.auth.accessToken;
+
+  if (accessToken) {
+    config.headers = config.headers ?? {};
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
 });
